7c1801480d89600eda7446492aa3f35a88495ede
Dylan Patrick Clark <dylanclark112@gmail.com>
2013-04-03 21:10:55 -0700

    Update rental_request.rb

--- cats99/app/models/rental_request.rb
+++ cats99/app/models/rental_request.rb
@@ -8,8 +8,8 @@ class RentalRequest < ActiveRecord::Base
   def rental_period_does_not_overlap
     overlaps = RentalRequest.where("cat_id = ? AND
                         status = 1 AND
-                        ((start_time BETWEEN ? AND ? ) OR
-                        (end_time BETWEEN ? AND ?))",
+                        ((start_time BETWEEN ? AND ? ) OR #REV: for less comparisons, you could also say something like
+                        (end_time BETWEEN ? AND ?))",#REV: !(start_time < self.end_time or end_time > self.start_time)
                           self.cat_id,
                           self.start_time,
                           self.end_time,
