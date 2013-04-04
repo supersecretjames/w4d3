7a0c17ce7aac07b936f1352244ad7dcbfc648969
Dylan Patrick Clark <dylanclark112@gmail.com>
2013-04-03 21:11:35 -0700

    Update cat.rb

--- cats99/app/models/cat.rb
+++ cats99/app/models/cat.rb
@@ -2,6 +2,6 @@ class Cat < ActiveRecord::Base
   attr_accessible :name, :age, :birth_date, :color_id, :location_id, :sex
 
   belongs_to :location
-  belongs_to :color
+  belongs_to :color #REV: you may consider just inferring age from birth date. 
   has_many :rental_requests
 end
